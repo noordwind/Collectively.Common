5908d653756af7b64d655fee4cd1f5003f39d9f2
Piotr Gankiewicz <spetz90@gmail.com>
2016-10-24 13:32:56 +0200

    Added name to remark file

--- Events/Remarks/Models/RemarkFile.cs
+++ Events/Remarks/Models/RemarkFile.cs
@@ -2,14 +2,16 @@
 {
     public class RemarkFile
     {
+        public string Name { get; }
         public string Size { get; }
         public string Url { get; }
         public string Metadata { get; }
         
         protected RemarkFile() {}
 
-        public RemarkFile(string size, string url, string metadata)
+        public RemarkFile(string name, string size, string url, string metadata)
         {
+            Name = name;
             Size = size;
             Url = url;
             Metadata = metadata;
