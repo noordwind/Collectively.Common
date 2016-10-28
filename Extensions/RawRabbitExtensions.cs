6b118be19d669c194ce92a4ccfc5f2d39375d42f
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2016-10-28 13:56:15 +0200

    queueName added into useRabbitMq method

--- Extensions/RawRabbitExtensions.cs
+++ Extensions/RawRabbitExtensions.cs
@@ -9,15 +9,18 @@ namespace Coolector.Common.Extensions
     public static class RawRabbitExtensions
     {
         public static ISubscription WithCommandHandlerAsync<TCommand>(this IBusClient bus,
-            ICommandHandler<TCommand> handler) where TCommand : ICommand
-            => bus.SubscribeAsync<TCommand>(async (msg, context) => await handler.HandleAsync(msg),
-                 cfg => cfg.WithQueue(q => q.WithName(GetExchangeName<TCommand>())));
+            ICommandHandler<TCommand> handler, string name = null) where TCommand : ICommand
+        => bus.SubscribeAsync<TCommand>(async (msg, context) => await handler.HandleAsync(msg),
+            cfg => cfg.WithQueue(q => q.WithName(GetExchangeName<TCommand>(name))));
 
         public static ISubscription WithEventHandlerAsync<TEvent>(this IBusClient bus,
-            IEventHandler<TEvent> handler) where TEvent : IEvent
-            => bus.SubscribeAsync<TEvent>(async (msg, context) => await handler.HandleAsync(msg), 
-                cfg => cfg.WithQueue(q => q.WithName(GetExchangeName<TEvent>())));
+            IEventHandler<TEvent> handler, string name = null) where TEvent : IEvent
+        => bus.SubscribeAsync<TEvent>(async (msg, context) => await handler.HandleAsync(msg),
+            cfg => cfg.WithQueue(q => q.WithName(GetExchangeName<TEvent>(name))));
 
-        private static string GetExchangeName<T>() => $"{Assembly.GetEntryAssembly().GetName().Name}/{typeof(T).Name}";
+        private static string GetExchangeName<T>(string name = null)
+            => string.IsNullOrWhiteSpace(name)
+                ? $"{Assembly.GetEntryAssembly().GetName()}/{typeof(T).Name}"
+                : $"{name}/{typeof(T).Name}";
     }
 }
