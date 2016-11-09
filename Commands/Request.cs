4cc264bfd4000f00400f63eabaa40aa0b2b23f06
spetz <spetz90@gmail.com>
2016-11-09 14:04:05 +0100

    Request name

--- Commands/Request.cs
+++ Commands/Request.cs
@@ -5,6 +5,7 @@ namespace Coolector.Common.Commands
     public class Request
     {
         public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
         public string Origin { get; set; }
         public string Resource { get; set; }
         public DateTime CreatedAt { get; set; }
