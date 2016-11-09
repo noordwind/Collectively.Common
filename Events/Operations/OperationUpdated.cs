4769111ee138abffa9a201f58fa59ce0ba72c6e2
spetz <spetz90@gmail.com>
2016-11-09 14:13:58 +0100

    Removed operation name from updated event

--- Events/Operations/OperationUpdated.cs
+++ Events/Operations/OperationUpdated.cs
@@ -5,7 +5,6 @@ namespace Coolector.Common.Events.Operations
     public class OperationUpdated : IAuthenticatedEvent
     {
         public Guid RequestId { get; }
-        public string Name { get; }
         public string UserId { get; }
         public string State { get; }
         public DateTime UpdatedAt { get; }
@@ -16,13 +15,11 @@ namespace Coolector.Common.Events.Operations
         }
 
         public OperationUpdated(Guid requestId,
-            string name,
             string userId, string state,
             DateTime updatedAt, 
             string message)
         {
             RequestId = requestId;
-            Name = name;
             UserId = userId;
             State = state;
             UpdatedAt = updatedAt;
