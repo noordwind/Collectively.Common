e83a891ded1c2efd1378295d319ee45416475a8b
spetz <spetz90@gmail.com>
2016-11-16 10:01:24 +0100

    Event for rejected sign up

---
+++ Events/Users/UserSignUpRejected.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Coolector.Common.Events.Users
+{
+    public class UserSignUpRejected : IRejectedEvent
+    {
+        public Guid RequestId { get; }
+        public string UserId { get; }
+        public string Reason { get; }
+        public string Provider { get; }
+
+        protected UserSignUpRejected()
+        {
+        }
+
+        public UserSignUpRejected(Guid requestId, string userId,
+            string reason, string provider)
+        {
+            RequestId = requestId;
+            UserId = userId;
+            Reason = reason;
+            Provider = provider;
+        }
+    }
+}
