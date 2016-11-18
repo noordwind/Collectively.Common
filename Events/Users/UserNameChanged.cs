7a264b30573141d75b25ddd03b286882fc4cc3bf
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2016-11-18 10:35:36 +0100

    state added to the usernamechanged event

--- Events/Users/UserNameChanged.cs
+++ Events/Users/UserNameChanged.cs
@@ -7,16 +7,18 @@ namespace Coolector.Common.Events.Users
         public Guid RequestId { get; }
         public string UserId { get; }
         public string NewName { get; }
+        public string State { get; }
 
         protected UserNameChanged()
         {
         }
 
-        public UserNameChanged(Guid requestId, string userId, string newName)
+        public UserNameChanged(Guid requestId, string userId, string newName, string state)
         {
             RequestId = requestId;
             UserId = userId;
             NewName = newName;
+            State = state;
         }
     }
 }
