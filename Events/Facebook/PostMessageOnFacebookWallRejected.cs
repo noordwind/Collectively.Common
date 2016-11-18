6895f4d5041cfe5573fb0b7be61ba30c21539e89
spetz <spetz90@gmail.com>
2016-11-18 11:37:52 +0100

    Facebook wall posting command & event

---
+++ Events/Facebook/PostMessageOnFacebookWallRejected.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Coolector.Common.Events.Facebook
+{
+    public class PostMessageOnFacebookWallRejected : IRejectedEvent
+    {
+        public Guid RequestId { get; }
+        public string UserId { get; }
+        public string Reason { get; }
+        public string Message { get; }
+
+        protected PostMessageOnFacebookWallRejected()
+        {
+        }
+
+        public PostMessageOnFacebookWallRejected(Guid requestId, string userId,
+            string reason, string message)
+        {
+            RequestId = requestId;
+            UserId = userId;
+            Reason = reason;
+            Message = message;
+        }
+    }
+}
