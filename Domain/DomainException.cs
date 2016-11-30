e5938a67dff9ee412c4a14bf02296a6866c87ac7
spetz <spetz90@gmail.com>
2016-11-30 10:23:05 +0100

    Code in exceptions

--- Domain/DomainException.cs
+++ Domain/DomainException.cs
@@ -4,17 +4,34 @@ namespace Coolector.Common.Domain
 {
     public class DomainException : Exception
     {
+        public string Code { get; }
+
         public DomainException()
         {
         }
 
-        public DomainException(string message, params object[] args) : this(null, message, args)
+        public DomainException(string code)
+        {
+            Code = code;
+        }
+
+        public DomainException(string message, params object[] args) : this(string.Empty, message, args)
+        {
+        }
+
+        public DomainException(string code, string message, params object[] args) : this(null, code, message, args)
         {
         }
 
         public DomainException(Exception innerException, string message, params object[] args)
+            : this(innerException, string.Empty, message, args)
+        {
+        }
+
+        public DomainException(Exception innerException, string code, string message, params object[] args)
             : base(string.Format(message, args), innerException)
         {
+            Code = code;
         }
     }
 }
