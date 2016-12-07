1fc3878c528066d22811538581d56c99699e48bf
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2016-12-07 10:08:50 +0100

    some rejected events added

---
+++ Coolector.Common/Events/Users/ChangeAvatarRejected.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Coolector.Common.Events.Users
+{
+    public class ChangeAvatarRejected : IRejectedEvent
+    {
+        public Guid RequestId { get; }
+        public string UserId { get; }
+        public string Code { get; }
+        public string Reason { get; }
+
+
+        protected ChangeAvatarRejected()
+        {
+            
+        }
+
+        public ChangeAvatarRejected(Guid requestId, 
+            string userId, string code, string reason)
+        {
+            RequestId = requestId;
+            UserId = userId;
+            Code = code;
+            Reason = reason;
+        }
+    }
+}
---
+++ Coolector.Common/Events/Users/SignOutRejected.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Coolector.Common.Events.Users
+{
+    public class SignOutRejected : IRejectedEvent
+    {
+        public Guid RequestId { get; }
+        public string UserId { get; }
+        public string Code { get; }
+        public string Reason { get; }
+
+        protected SignOutRejected() { }
+
+        public SignOutRejected(Guid requestId, string userId, string code, string reason)
+        {
+            RequestId = requestId;
+            UserId = userId;
+            Code = code;
+            Reason = reason;
+        }
+    }
+}
