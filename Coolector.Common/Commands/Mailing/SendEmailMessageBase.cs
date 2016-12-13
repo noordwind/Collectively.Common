0619d3d9f6ee7427d3344ab7e85de526de7ce9be
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2016-12-13 13:46:56 +0100

    mailing service commands moved to its shared project

--- Coolector.Common/Commands/Mailing/SendEmailMessageBase.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Coolector.Common.Commands.Mailing
-{
-    public abstract class SendEmailMessageBase : ICommand
-    {
-        public Request Request { get; set; }
-        public string Email { get; set; }
-    }
-}
--- Coolector.Common/Commands/Mailing/SendResetPasswordEmailMessage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Coolector.Common.Commands.Mailing
-{
-    public class SendResetPasswordEmailMessage : SendEmailMessageBase
-    {
-        public string Token { get; set; }
-        public string Endpoint { get; set; }
-    }
-}
