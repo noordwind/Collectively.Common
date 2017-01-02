75c9bfd44d01e6d0ef8e04815255a5624c494da9
spetz <spetz90@gmail.com>
2017-01-02 09:36:53 +0100

    RabbitMQ container

---
+++ Coolector.Common/RabbitMq/RabbitMqContainer.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+using Autofac;
+using NLog;
+using Polly;
+using RabbitMQ.Client.Exceptions;
+using RawRabbit;
+using RawRabbit.Configuration;
+using RawRabbit.vNext;
+
+namespace Coolector.Common.RabbitMq
+{
+    public static class RabbitMqContainer
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public static void Register(ContainerBuilder builder, RawRabbitConfiguration configuration, int retryAttempts = 5)
+        {
+            var policy = Policy
+                .Handle<ConnectFailureException>()
+                .Or<BrokerUnreachableException>()
+                .Or<IOException>()
+                .WaitAndRetry(retryAttempts, retryAttempt =>
+                            TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    (exception, timeSpan, retryCount, context) =>
+                    {
+                        Logger.Error(exception, "Can not connect to RabbitMQ. " +
+                                                $"Retries: {retryCount}, duration: {timeSpan}");
+                    }
+                );
+
+            builder.RegisterInstance(configuration).SingleInstance();
+            policy.Execute(() => builder
+                    .RegisterInstance(BusClientFactory.CreateDefault(configuration))
+                    .As<IBusClient>()
+            );
+        }
+    }
+}
