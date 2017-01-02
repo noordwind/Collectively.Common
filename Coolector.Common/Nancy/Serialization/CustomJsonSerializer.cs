b08b88b8b1afe8e712cb65e65f100ea0f283ec5b
spetz <spetz90@gmail.com>
2017-01-02 08:58:19 +0100

    Nancy serialization

---
+++ Coolector.Common/Nancy/Serialization/CustomJsonSerializer.cs
@@ -0,0 +1,22 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Coolector.Common.Nancy.Serialization
+{
+    public sealed class CustomJsonSerializer : JsonSerializer
+    {
+        private const string DateTimeFormat = "yyyy-MM-dd H:mm:ss";
+
+        public CustomJsonSerializer()
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver();
+            Formatting = Formatting.Indented;
+            DateFormatString = DateTimeFormat;
+            Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter
+            {
+                AllowIntegerValues = true,
+                CamelCaseText = true
+            });
+        }
+    }
+}
