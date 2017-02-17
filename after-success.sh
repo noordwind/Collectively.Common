609d8fad7aa55039d660d9a87d1e34fefebe407b
spetz <spetz90@gmail.com>
2017-02-17 11:54:34 +0100

    Updated travis and readme

---
+++ after-success.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+case "$TRAVIS_BRANCH" in
+  "master")
+    echo Triggering MyGet package build using branch $TRAVIS_BRANCH
+    curl -X POST -d '{}' "$MYGET_TRIGGER_BUILD_PACKAGE_URL"
+    ;;
+  "develop")
+    echo Triggering MyGet package build using branch $TRAVIS_BRANCH
+    curl -X POST -d '{}' "$MYGET_TRIGGER_BUILD_PACKAGE_DEV_URL"
+    ;;    
+esac
