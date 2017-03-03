4f86c8709f41addd02cb7698fe7fafb19d738a9b
spetz <spetz90@gmail.com>
2017-03-03 10:49:58 +0100

    Fixed myget config

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-dotnet restore --source "https://api.nuget.org/v3/index.json" --source "https://www.myget.org/F/collectively/api/v3/index.json" --no-cache
+dotnet restore --source "https://api.nuget.org/v3/index.json" --source "https://www.myget.org/F/collectively%build-env%/api/v3/index.json" --no-cache
 dotnet pack "Collectively.Common" -o .
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-dotnet restore --source "https://api.nuget.org/v3/index.json" --source "https://www.myget.org/F/collectively/api/v3/index.json" --no-cache
+dotnet restore --source "https://api.nuget.org/v3/index.json" --source "https://www.myget.org/F/collectively%build-env%/api/v3/index.json" --no-cache
 dotnet pack "Collectively.Common" -o .
