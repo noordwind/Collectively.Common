41b5f6dcd7eb4ce43a760c29ada88d66b5f7e013
spetz <spetz90@gmail.com>
2017-03-15 13:56:36 +0100

    Fixed dotnet restore

--- travis-build.sh
+++ travis-build.sh
@@ -6,7 +6,7 @@ case "$TRAVIS_BRANCH" in
     ;;
 esac
 
-PROJECTS=(Collectively.Common Collectively.Common)
+PROJECTS=(Collectively.Common Collectively.Common.Tests)
 for PROJECT in ${PROJECTS[*]}
 do
   dotnet restore $PROJECT --source "https://api.nuget.org/v3/index.json" --source "https://www.myget.org/F/collectively$MYGET_ENV/api/v3/index.json" --no-cache
