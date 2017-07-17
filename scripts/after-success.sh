b6a55ebfb6083be6921c4ea847a49f00960a3f36
Piotr Gankiewicz <spetz90@gmail.com>
2017-07-17 13:55:22 +0200

    Updated after success script

--- scripts/after-success.sh
+++ scripts/after-success.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo Executing after success scripts on branch $TRAVIS_BRANCH
+echo Triggering MyGet package build
+
 case "$TRAVIS_BRANCH" in
   "master")
     echo Triggering MyGet package build using branch $TRAVIS_BRANCH
