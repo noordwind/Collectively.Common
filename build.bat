f365dab9f43aa3292c4fa25ca4d8239959850991
Piotr Gankiewicz <spetz90@gmail.com>
2017-08-19 17:20:57 +0200

    Fixed dotnet pack

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 cd src/Collectively.Common
 dotnet restore --source "https://api.nuget.org/v3/index.json" --source "https://www.myget.org/F/collectively%build-env%/api/v3/index.json" --no-cache
-dotnet pack -o .
+dotnet pack --no-restore -o .
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd src/Collectively.Common
 dotnet restore --source "https://api.nuget.org/v3/index.json" --source "https://www.myget.org/F/collectively%build-env%/api/v3/index.json" --no-cache
-dotnet pack -o .
+dotnet pack --no-restore -o .
