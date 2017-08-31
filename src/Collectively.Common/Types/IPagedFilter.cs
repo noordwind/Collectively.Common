ef799937b26066cb6b53126e84f99010b3e6acf1
Piotr Gankiewicz <spetz90@gmail.com>
2017-08-31 15:15:46 +0200

    Paged filter type

---
+++ src/Collectively.Common/Types/IPagedFilter.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Collectively.Common.Queries;
+
+namespace Collectively.Common.Types
+{
+    public interface IPagedFilter<TResult, in TQuery> where TQuery : IQuery
+    {
+        PagedResult<TResult> Filter(IEnumerable<TResult> values, TQuery query);
+    }
+}
