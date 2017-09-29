94190ffcb2ed8f9980cb253bca8b4c5b51ad7674
Piotr Gankiewicz <spetz90@gmail.com>
2017-09-29 15:32:49 +0200

    Location service retry policy

--- src/Collectively.Common/Locations/LocationService.cs
+++ src/Collectively.Common/Locations/LocationService.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Collectively.Common.Extensions;
 using Collectively.Common.Types;
 using Newtonsoft.Json;
+using Polly;
 
 namespace Collectively.Common.Locations
 {
@@ -30,14 +31,24 @@ namespace Collectively.Common.Locations
 
         public async Task<Maybe<LocationResponse>> GetAsync(string address, double latitude, double longitude)
         {
-            var query = address.Empty() ? $"latlng={latitude},{longitude}" : $"address={address}";
-            var queryWithKey = _settings.ApiKey.Empty() ? query : $"{query}&key={_settings.ApiKey}";
-            var response = await _client.GetAsync($"?{queryWithKey}");
+            var retries = _settings.RetriesCount <= 0 ? 0 : _settings.RetriesCount;
+            var retriesDelay = _settings.RetriesDelay <= 0 ? 500 : _settings.RetriesDelay;
+            var retryPolicy = Policy
+                .HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+                .WaitAndRetryAsync<HttpResponseMessage>(retries, 
+                    retryAttempt => TimeSpan.FromMilliseconds(retriesDelay));
+            var response = await retryPolicy.ExecuteAsync(async () => 
+            {
+                var query = address.Empty() ? $"latlng={latitude},{longitude}" : $"address={address}";
+                var queryWithKey = _settings.ApiKey.Empty() ? query : $"{query}&key={_settings.ApiKey}";
+
+                return await _client.GetAsync($"?{queryWithKey}");
+            });
+            var content = await response.Content.ReadAsStringAsync();
             if(!response.IsSuccessStatusCode)
             {
                 return null;
             }
-            var content = await response.Content.ReadAsStringAsync();
 
             return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<LocationResponse>(content));
         }
--- src/Collectively.Common/Locations/LocationSettings.cs
+++ src/Collectively.Common/Locations/LocationSettings.cs
@@ -3,5 +3,7 @@ namespace Collectively.Common.Locations
     public class LocationSettings
     {
         public string ApiKey { get; set ;}
+        public int RetriesCount { get; set ;}
+        public int RetriesDelay { get; set ;}
     }
 }
